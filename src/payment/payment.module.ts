bb2c2f1232a18ea4c1290dd0ff5448739879e2c1
anthoni portocarrero rodriguez <softwaretoni21@gmail.com>
2025-06-27 01:41:10 -0500

    feat: refactor payment module by adding UserPaymentsController and UserPaymentsService, and reorganizing PaymentController and PaymentService

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PaymentController } from './controllers/payment.controller';
+import { UserPaymentsController } from './controllers/user-payment.controller';
 import { PaymentConfig } from './entities/payment-config.entity';
 import { PaymentItem } from './entities/payment-item.entity';
 import { Payment } from './entities/payment.entity';
-import { PaymentController } from './payment.controller';
-import { PaymentService } from './payment.service';
+import { PaymentService } from './services/payment.service';
+import { UserPaymentsService } from './services/user-payment.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Payment, PaymentConfig, PaymentItem])],
-  controllers: [PaymentController],
-  providers: [PaymentService],
-  exports: [TypeOrmModule, PaymentService],
+  controllers: [PaymentController, UserPaymentsController],
+  providers: [PaymentService, UserPaymentsService],
+  exports: [TypeOrmModule, PaymentService, UserPaymentsService],
 })
 export class PaymentModule {}
--- src/payment/services/payment.service.ts
+++ src/payment/services/payment.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Payment } from './entities/payment.entity';
+import { Payment } from '../entities/payment.entity';
 
 @Injectable()
 export class PaymentService {
